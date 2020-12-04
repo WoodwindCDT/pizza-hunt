b7989baa1e476ad0da58817c6ce8f447efb2bd00
Ender <woodwind.turbeville@gmail.com>
2020-12-03 22:22:28 -0600

    Added new models, js, and routing for comment relation, pizza addition, and enabled data to show on homepage

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,21 +1,31 @@
 const { Pizza } = require('../models');
 
 const pizzaController = {
-  // get all pizzas GET All
+  // To get all pizzas GET All
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
         res.status(400).json(err);
       });
-    },
+  },
 
-  // get one pizza by id GET @ ID
+  // To get one pizza by id GET @ ID
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
       .then(dbPizzaData => {
-        // If no pizza is found, send 404
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
           return;
@@ -26,7 +36,7 @@ const pizzaController = {
         console.log(err);
         res.status(400).json(err);
       });
-    },
+  },
 
     // createPizza POST
     createPizza({ body }, res) {
